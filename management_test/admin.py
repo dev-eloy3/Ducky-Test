e4f5ba69e7d699b2c51eda35774777ba53f06f3f
eloy <eloydominguez3@proton.me>
2025-07-15 12:14:19 +0200

    borrado

--- management_test/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import TestResult
-
-# admin.site.register(TestResult)
-
--- management_test/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-     path('', views.enseñar_test, name='ver el test'),
-     path('guardar_respuestas/', views.guardar_respuestas_view, name='guardar_respuestas'),
- ]
-   
